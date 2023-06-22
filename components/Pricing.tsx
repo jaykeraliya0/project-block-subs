1f2c2c8d52d3bf698f6d2300cc96ad4993f643e2
Jay Keraliya <jaykeraliya0@gmail.com>
2023-06-22 11:59:29 +0530

    Fix build errors Fixes #3

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -70,7 +70,7 @@ export default function Pricing() {
               </div>
               <div className="px-6 pt-6 pb-8">
                 <h3 className="text-sm font-medium text-gray-900">
-                  What's included
+                  What&apos;s included
                 </h3>
                 <ul role="list" className="mt-6 space-y-4">
                   {tier.includedFeatures.map((feature) => (
